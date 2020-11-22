435830de97ac0fddaf146207dce95c2cff271547
diego <diego@192.168.0.5>
2020-11-22 16:45:39 -0500

    clase principal editada y pom.xml page

--- ServicioController/src/main/java/com/example/ServicioControllerApplication.java
+++ ServicioController/src/main/java/com/example/ServicioControllerApplication.java
@@ -6,14 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 //@ComponentScan({"com.example"})
 
 @SpringBootApplication
-public class ServicioControllerApplication {
+public class ServicioControllerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioControllerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServicioControllerApplication.class);
 	}
 
 }
